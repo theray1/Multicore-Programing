7aa9b116854fc21778e63b7d41a5b83773428c1d
Erwan Boisteau-Desdevises <erwanbd@outlook.fr>
2023-04-06 12:27:12 +0200

    Added TP3

--- src/fr/univnantes/multicore/tp3/WebGrep.java
+++ src/fr/univnantes/multicore/tp3/WebGrep.java
@@ -1,35 +1,48 @@
 package fr.univnantes.multicore.tp3;
 
+import javax.tools.Tool;
+import java.beans.Expression;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.concurrent.*;
 
 public class WebGrep {
 
-	private final static LinkedList<String> explored = new LinkedList<String>();
+	//private final static LinkedList<String> explored = new LinkedList<String>();
+
+	private final static ConcurrentSkipListSet<String> explored = new ConcurrentSkipListSet<>();
+
+	private final static ConcurrentLinkedQueue<ParsedPage> toRead = new ConcurrentLinkedQueue<ParsedPage>();
+
+	private final static ExecutorService threadPool = Executors.newFixedThreadPool(1000);
 
 	/*
 	 *  TODO : the search must be parallelized between the given number of threads
 	 */
 	private static void explore(String address) {
-		try {
-			/*
-			 *  Check that the page was not already explored and adds it
-			 * TODO : the check and insertion must be atomic. Explain why. How to do it?
-			 */
-			if(!explored.contains(address)) {
-				explored.add(address);
-				// Parse the page to find matches and hypertext links
-				ParsedPage page = Tools.parsePage(address);
-				if(!page.matches().isEmpty()) {
-					/* 
-					 * TODO: Tools.print(page) is not thread safe...
-					 */
-					Tools.print(page);
-					// Recursively explore other pages
-					for(String href : page.hrefs()) explore(href);
+		if(explored.add(address))
+		threadPool.submit(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					//System.out.println(address + " was added to the explored list");
+					ParsedPage page = Tools.parsePage(address);
+					if(!page.matches().isEmpty()){
+							toRead.add(page);
+							//System.out.println(address + " was added to the to read list");
+							for(String href : page.hrefs()) explore(href);
+					}
+
+				}catch (FileNotFoundException e){
+					//explore(address);
+					//e.printStackTrace();
+				}catch (Exception e){
+					e.printStackTrace();
 				}
 			}
-		} catch (Exception e) {/*We could retry later...*/}
+		});
 	}
 
 
@@ -39,10 +52,32 @@ public class WebGrep {
 		else Tools.initialize(args);
 
 		// TODO Just do it!
-		System.err.println("You must parallelize the application between " + Tools.numberThreads() + " threads!\n");
+		System.err.println("You must search parallelize the application between " + Tools.numberThreads() + " threads!\n");
+
+		//Consumer Thread for printing pages
+		threadPool.submit(new Runnable() {
+			@Override
+			public void run() {
+				while(true){
+					if(!toRead.isEmpty()){
+						Tools.print(toRead.poll());
+						//toRead.poll();
+						//System.out.println("size of toRead: " + toRead.size());
+					}
+				}
+			}
+		});
 
 		// Get the starting URL given in argument
 		for(String address : Tools.startingURL()) 
 			explore(address);
+
+		if(threadPool.awaitTermination(10, TimeUnit.SECONDS)){
+			System.out.println("Termination occured");
+		}else {
+			System.out.println("Waited 10 seconds for threads to complete execution; didn't happen");
+		}
+
+		System.out.println("explored " + explored.size() + " pages");
 	}
 }
